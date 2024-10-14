0d851a0acd012fd26ea6a1eb51c1e57ab612cd4a
KirillKrutski <kirillevgrafov2003@gmail.com>
2024-10-14 14:26:29 +0300

    add comments in code

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,22 @@ use std::path::Path;
 
 
 fn list_dir(path: &Path) -> io::Result<()>{
-    if path.is_dir(){
-        for entry in fs::read_dir(path)? {
+    if path.is_dir(){ // идем по путю
+        for entry in fs::read_dir(path)? { // читаем директорию и идем по файлам
             let entry = entry?;
             let file_name = entry.file_name();
             println!("{}", file_name.to_string_lossy());
         }
     } else {
-        println!("{} isn't a dir", path.display());
+        println!("{} isn't a dir", path.display()); // обратботка ошибки в случаем, если директория не существует
     }
-    Ok(())
+    Ok(()) // конец цикла
 }
 
 
 fn main() -> io::Result<()>{
     let args: Vec<String> = env::args().collect();
-    let path = if args.len() > 1{
+    let path = if args.len() > 1{ // проверяем задал ли пользователь саму директорию, если нет, то будет запущена директория по умолчанию
         Path::new(&args[1])
     } else {
         Path::new(".")
